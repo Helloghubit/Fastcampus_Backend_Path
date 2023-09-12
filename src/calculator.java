688a33f6e354ce0612cce5a95dc7c444ff25761e
devtaek <rnjsxorals1@gmail.com>
2023-09-12 19:45:41 +0900

    2

---
+++ src/calculator.java
@@ -0,0 +1,12 @@
+public class calculator {
+    public static void main (String[] args){
+        int a, b;
+        a = 90;
+        b = 30;
+        System.out.println(a + "+" + b + "=" + (a + b));
+        System.out.println(a + "-" + b + "=" + (a - b));
+        System.out.println(a + "/" + b + "=" + (a / b));
+        System.out.println(a + "*" + b + "=" + (a * b));
+
+    }
+}
