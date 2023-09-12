1ec550ddc0ccfc812e3b3bd5a1a4d45d75ab8ff5
devtaek <rnjsxorals1@gmail.com>
2023-09-12 19:46:47 +0900

    3

--- src/operator.java
+++
@@ -1,8 +0,0 @@
-public class operator {
-    public static void main(String[] args) {
-        int kor = 77;
-        int eng = 87;
-        System.out.println("총점 : " + (kor + eng));
-        System.out.println("평균 : " + (kor + eng) / 2);
-    }
-}
